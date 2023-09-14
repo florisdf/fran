0e6428bdf1dcbf54dbddfaf3f0c7a643cf6be90b
Floris <floris.defeyter@kuleuven.be>
2023-09-14 17:49:41 +0000

    Add transforms for FRAN dataset

--- transforms/__init__.py
+++
@@ -1,2 +0,0 @@
-from .age_transform import AgeTransform, add_aging_channel  # noqa
-from .test_transform import test_transform  # noqa
---
+++ transforms/fran_transforms.py
@@ -0,0 +1,45 @@
+from typing import Callable, List, Tuple
+
+import torch
+from torchvision.transforms import CenterCrop, Compose, ToTensor, Normalize, \
+    RandomCrop, ColorJitter, RandomRotation
+
+
+def data_transforms(
+    crop_size: int,
+    norm_mean: List[float],
+    norm_std: List[float],
+    jitter_brightness: float,
+    jitter_contrast: float,
+    jitter_saturation: float,
+    jitter_hue: float,
+    random_angle: float,
+) -> Tuple[Callable, Callable]:
+    """Creates the data transforms for FRAN training and validation."""
+
+    train_tfms = [
+        ToTensor(),
+        RandomCrop(crop_size),
+        Normalize(mean=norm_mean, std=norm_std)
+        ColorJitter(
+            brightness=jitter_brightness,
+            contrast=jitter_contrast,
+            saturation=jitter_saturation,
+            hue=jitter_hue,
+        )
+    ]
+
+    if random_angle != 0:
+        train_tfms.append(
+            RandomRotation(random_angle)
+        )
+
+    tfm_train = Compose(train_tfms)
+
+    tfm_val = Compose([
+        ToTensor(),
+        CenterCrop(crop_size),
+        Normalize(mean=norm_mean, std=norm_std)
+    ])
+
+    return tfm_train, tfm_val
